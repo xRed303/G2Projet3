f3fecb4992c996aa1ce3a985cc75421d7f271bef
safwene2007 <safwene2007@gmail.com>
2025-11-06 17:32:19 +0100

    Add files via upload

---
+++ exercice.py
@@ -0,0 +1,17 @@
+def decoder(s):
+    s.strip("|")
+    s[0] == "1"
+    a = s[0]
+    s[2] == "5"
+    b = s[2]
+    s[4:] == "Hello"
+    c = s[4:]
+    display.scroll(a, b, c)
+
+def choisir_forme(s)
+if s[0] == "1":
+    display.show(Image.SQUARE)
+if s[0] == "2":
+    display.show(Image.HEART)
+    
+
