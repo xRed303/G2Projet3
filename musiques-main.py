078cfd78422aac205a8b0e3e78353cabdbee1d14
mattculot <matt.culot@student.uclouvain.be>
2025-11-20 16:40:44 +0100

    Add files via upload

---
+++ musiques-main.py
@@ -0,0 +1,25 @@
+import music
+
+def musique():
+    tune = [
+    "R:4", "C4:2","D:2","E:4", "C:2","R:2","C:2","D:2","E:4","C:2", "R:2",
+    "C4:2","E:2","A#:4","A:4","G:4","G:4","F:4",
+    "G#3:2","A#:2","B:4", "G#:2","R:2","G#:2","A#:2","B:4","G#:2", "R:2",
+    "G#:2", "B:4", "D3:4", "G:4", #première partie
+    ]
+
+    music.play(tune)
+
+def musique2():
+    tune = [
+    "G#3:2","A#:2","B:4", "G#:2","R:2","G#:2","A#:2","B:4","G#:2", "R:2",
+    "G#:2", "B:2", "F4:2", "E:2", "C:2" #deuxième partie
+    ]
+    music.play(tune)
+musique()
+musique2()
+
+
+
+#faire une comptine pour quand il est agité
+
