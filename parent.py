c64c6ba63b534f2b8ba7fac07aa871c5d478d900
nadarn07 <nadarn18@icloud.com>
2025-12-06 19:38:07 +0100

    Update parent.py

--- parent.py
+++ parent.py
@@ -183,6 +183,7 @@ def StatusChute(data):
     display.scroll("Chute")
 
 
+
 ################################################################################################
 display.show(Image.SQUARE)
 radio.on()
